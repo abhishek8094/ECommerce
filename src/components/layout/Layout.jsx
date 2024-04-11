c0e2a07aac393670f5d1d9cabe203ec76c3e5619
abhisek8094 <iasabhi41@gmail.com>
2024-04-11 19:33:48 +0530

    update layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,11 +4,11 @@ import Footer from "../footer/Footer";
 
 function Layout({ children }) {
   return (
-    <div>
+    <>
       <Navbar />
       <div className="content">{children}</div>
       <Footer />
-    </div>
+    </>
   );
 }
 
